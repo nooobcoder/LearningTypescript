411dcd0e39f32e586255d973cf58dcdbc3b341ad
nooobcoder <suryashi2013@gmail.com>
2021-07-17 12:42:55 +0530

    Built in generics

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,11 @@
+"use strict";
 // Built in generics
-const names: Array<string> = ["Max", "Ankur"]; // string[]
+var names = ["Max", "Ankur"]; // string[]
 console.log(names[0].split(" "));
-
 // const promise: Promise<string> = new Promise((resolve) => {
 // 	setTimeout(() => {
 // 		resolve("This is done!");
 // 	}, 2000);
 // });
 // promise.then((data) => data.split(" "));
+//# sourceMappingURL=main.js.map
