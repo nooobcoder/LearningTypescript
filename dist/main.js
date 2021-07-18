df28a8ea6d01730904e4254983d52275f5bac7c4
nooobcoder <suryashi2013@gmail.com>
2021-07-18 12:47:32 +0530

    Added event listeners to form elements

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,23 @@
 "use strict";
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
+        var _this = this;
+        this.submitHandler = function (event) {
+            event === null || event === void 0 ? void 0 : event.preventDefault();
+            console.log(_this.titleInputElement.value);
+        };
+        this.configureEventListeners = function () {
+            _this.element.addEventListener("submit", _this.submitHandler.bind(_this));
+        };
         this.templateElement = (document.getElementById("project-input"));
         this.hostElement = document.getElementById("app");
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = "user-input"; // Assigning an id to give styling.
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInptuElement = this.element.querySelector("#people");
+        this.configureEventListeners();
         this.attach();
     }
     ProjectInput.prototype.attach = function () {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
+// autobind decorator
+/* const autoBind = (_: any, _2: string, desciptor: PropertyDescriptor) => {
+	const originalMethod = desciptor.value;
+	const adjacentDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get: function () {
+			const boundFunction = originalMethod.bind(this);
+			return boundFunction;
+		},
+	};
+	return adjacentDescriptor;
+}; */
+
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInptuElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = <HTMLTemplateElement>(
@@ -14,8 +30,31 @@ class ProjectInput {
 			true
 		);
 		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = "user-input"; // Assigning an id to give styling.
+
+		this.titleInputElement = this.element.querySelector(
+			"#title"
+		) as HTMLInputElement;
+		this.descriptionInputElement = this.element.querySelector(
+			"#description"
+		) as HTMLInputElement;
+		this.peopleInptuElement = this.element.querySelector(
+			"#people"
+		) as HTMLInputElement;
+
+		this.configureEventListeners();
 		this.attach();
 	}
+
+	// @autoBind
+	private submitHandler = (event: Event) => {
+		event?.preventDefault();
+		console.log(this.titleInputElement.value);
+	};
+
+	private configureEventListeners = () => {
+		this.element.addEventListener("submit", this.submitHandler.bind(this));
+	};
 
 	private attach() {
 		this.hostElement.insertAdjacentElement("afterbegin", this.element);
