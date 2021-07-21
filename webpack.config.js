31ee031a244022cf79589ee4ec3d19bb704a0eda
nooobcoder <suryashi2013@gmail.com>
2021-07-21 20:39:41 +0530

    Added webpack as a dependency and configured it
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const config = {
+	entry: './src/main.ts',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'), // This should always be an absolute path
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+};
+
+module.exports = config;
