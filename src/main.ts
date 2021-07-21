e642bb1947f78867b8f535ffc8270f0d7be8f0c6
nooobcoder <suryashi2013@gmail.com>
2021-07-21 11:46:03 +0530

    Successfully moved to ES based exports
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
-// General syntax to import a namespace in TypeScript
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
 
-/**
- * Referencing the components from,
- *
- * components/project-input.ts
- * components/project-list.ts
- */
-/// <reference path="components/project-input.ts" />
-/// <reference path="components/project-list.ts" />
-
-namespace App {
-  new ProjectInput();
-  new ProjectList('active');
-  new ProjectList('finished');
-}
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
