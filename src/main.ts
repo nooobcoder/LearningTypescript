7becd46957a93d03421baca42539fd1be6cc7c25
nooobcoder <suryashi2013@gmail.com>
2021-07-24 11:31:18 +0530

    Snapshot
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
-import ldsh from 'lodash';
+import { Product } from './product.model';
 
-console.log(ldsh.shuffle([1, 10, 4, 2, -4, 2]));
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
+
+// @ts-ignore
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 10.99 },
+];
+
+// const loadedProducts = products.map((product) => new Product(product.title, product.price));
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+// const prod1 = new Product('A Book', 12.99);
+// console.log(prod1.getInformation());
