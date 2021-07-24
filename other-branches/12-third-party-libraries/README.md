7c93bc185cbc4de15f40514bf738af9511cfde3a
nooobcoder <suryashi2013@gmail.com>
2021-07-24 11:37:38 +0530

    Merge branch 12-third-party-libraries

---
+++ other-branches/12-third-party-libraries/README.md
@@ -0,0 +1,10 @@
+### Learning Typescript
+
+This repository hosts some tutorials while I learnt Typescript.
+
+Please checkout the branches of the repo as I have divided them individually.
+
+Course content is from: https://www.udemy.com/course/understanding-typescript/
+All thanks to: Maximilian Schwarzmuller (https://github.com/maxschwarzmueller)
+
+___
---
+++ other-branches/12-third-party-libraries/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const config = {
+  entry: './src/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // This should always be an absolute path
+    publicPath: '/dist/', // This is required for live reloading.
+  },
+  watch: true, // Watches for file changes and does not terminate.
+  devtool: 'inline-source-map',
+  mode: 'development',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+    historyApiFallback: true,
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
+
+module.exports = config;
