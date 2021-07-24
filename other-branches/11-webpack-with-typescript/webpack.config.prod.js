1027b9842242719b5bbcab44a3e9e16bab8899b1
nooobcoder <suryashi2013@gmail.com>
2021-07-24 10:59:44 +0530

    Webpack enabling live reload
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

--- other-branches/11-webpack-with-typescript/webpack.config.prod.js
+++ other-branches/11-webpack-with-typescript/webpack.config.prod.js
@@ -1,35 +1,35 @@
-const path = require("path");
-const CleanPlugin = require("clean-webpack-plugin");
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
 
 const config = {
-  mode: "production",
-  entry: "./src/main.ts",
+  mode: 'production',
+  entry: './src/main.ts',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist") // This should always be an absolute path
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // This should always be an absolute path
+    publicPath: '/dist/', // This is required for live reloading.
   },
+  watch: true, // Watches for file changes and does not terminate.
   devtool: false,
   devServer: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 3000,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
-  target: "web",
+  target: 'web',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        loader: "ts-loader",
-        exclude: /node_modules/
-      }
-    ]
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: ['.ts', '.js'],
   },
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin()
-  ]
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
 
 module.exports = config;
