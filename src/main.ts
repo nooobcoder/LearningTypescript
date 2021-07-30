5aaf5471732deff435f21c44cde817d9add6f8d5
nooobcoder <suryashi2013@gmail.com>
2021-07-30 21:24:17 +0530

    A class method for collections
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 3, name: 'newr name', age: 69 });
-
-user.on('save', () => console.log(user));
-
-user.save().then();
+const collection = User.buildUserCollection();
+collection.on('change', () => console.log(collection));
+collection.fetch().then();
