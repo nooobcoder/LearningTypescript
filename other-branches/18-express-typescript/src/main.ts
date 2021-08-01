e70a6914b2c7b3f5c6c31910401a56bb058cd34c
nooobcoder <suryashi2013@gmail.com>
2021-08-01 20:22:20 +0530

    Merge 18-express-typescript -> master
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

---
+++ other-branches/18-express-typescript/src/main.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { router } from './routes/loginRoutes.js';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+// Express 4.16+, express.urlencoded is the middleware to parse html bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['verysecretkey'] }));
+app.use(router);
+
+app.listen(3000, () => console.log('App is listening on port [3000]'));
