19d636697bc360b889ad80124d1503c50cf70da1
nooobcoder <suryashi2013@gmail.com>
2021-08-02 11:17:08 +0530

    Final commit
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,38 @@
+// Class Decorator https://www.typescriptlang.org/docs/handbook/decorators.html#class-decorators
+@classDecorator
 class Boat {
+
+  @testDecorator
   color: string = "red";
 
   @logError("Something bad")
-  pilot(): void {
-    throw new Error();
-    console.log("swoosh");
+  // Parameter Decorators https://www.typescriptlang.org/docs/handbook/decorators.html#parameter-decorators
+  pilot(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
+    console.log(generateWake);
+    if (speed === "fast") console.log("swoosh");
+    else console.log("nothing");
   }
 
+  @testDecorator
   get formattedColor(): string {
     return `This boat's color is ${this.color}`;
   }
 }
 
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor);
+}
+
+function parameterDecorator(_target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log("TARGET: ", target, "KEY: ", key);
+}
+
 // This wrap around is a decoratory factory
 function logError(errorMessage: string) {
-
   // This function is the actual decorator
   return function(_target: any, _key: string, propertyDescriptor: PropertyDescriptor): void {
     const method = propertyDescriptor.value;
@@ -30,5 +48,4 @@ function logError(errorMessage: string) {
 }
 
 // testDecorator(Boat.prototype,'pilot')
-
-new Boat().pilot();
+// new Boat().pilot();
