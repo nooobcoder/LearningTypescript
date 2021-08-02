cc3a5ff4af9d210f91bab2d4d1c35a87e20dd57e
nooobcoder <suryashi2013@gmail.com>
2021-08-02 21:02:30 +0530

    Initial React template commit
    Signed-off-by: nooobcoder <suryashi2013@gmail.com>

--- other-branches/16-football-game/dist/utils.js
+++
@@ -1,6 +0,0 @@
-var dateStringToDate = function (dateString) {
-    var dateParts = dateString.split('/').map(function (value) { return +value; });
-    return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
-};
-export { dateStringToDate };
-//# sourceMappingURL=utils.js.map
