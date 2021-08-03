17b197f1397e251d5975b3b974df87d5092b37f3
nooobcoder <suryashi2013@gmail.com>
2021-08-03 08:33:22 +0530

    Class based and function components

--- todos-app/src/index.tsx
+++ todos-app/src/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+interface AppProps {
+  color?: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  incrementCounter = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  decrementCounter = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.incrementCounter}>Increment</button>
+        <button onClick={this.decrementCounter}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}*/
+
+const App: React.VoidFunctionComponent<AppProps> = ({ color }: AppProps): JSX.Element => <div>{color}</div>;
+
+ReactDOM.render(<App color={'green'} />, document.getElementById('root'));
