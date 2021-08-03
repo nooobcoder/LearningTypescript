50386e95cc8f3d84ffb30b5b361261dad8829f7b
nooobcoder <suryashi2013@gmail.com>
2021-08-03 09:06:52 +0530

    Initial Redux setup

--- todos-app/src/index.tsx
+++ todos-app/src/index.tsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { App } from './components/App';
+import { reducers } from './reducers';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 interface AppProps {
   color?: string;
 }
 
-/*class App extends React.Component<AppProps> {
-  state = { counter: 0 };
-
-  incrementCounter = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-  decrementCounter = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.incrementCounter}>Increment</button>
-        <button onClick={this.decrementCounter}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}*/
-
-const App: React.FunctionComponent<AppProps> = ({ color }: AppProps): JSX.Element => <div>{color}</div>;
-
-ReactDOM.render(<App color={'green'} />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
